f953c523339bb0d085ed46fa5b6885eac1c93fa5
Leandro Gallac <gallacleandrog@gmail.com>
2025-02-11 11:21:20 -0300

    subiendo

---
+++ vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header />
+  <div>
+    <router-view/>
+  </div>
+  <Footer />
+</template>
+
+<script>
+import Header from "./components/App-Header";
+import Footer from "./components/App-Footer";
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-app/src/components/App-Login.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-3">Login</h1>
+                <hr>
+                <form-tag>
+                    
+                    <text-input 
+                        label="Email"
+                        type="email"
+                        name="email"
+                        required="true">
+                    </text-input>
+
+                    <text-input 
+                        label="Password"
+                        type="password"
+                        name="password"
+                        required="true">
+                    </text-input>  
+
+                    <hr>
+                    <input class="btn btn-primary" type="submit" value="Login">
+                </form-tag>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TextInput from './forms/TextInput.vue'
+import FormTag from './forms/FormTag.vue'
+
+export default {
+    name: 'AppLogin',
+    components: { 
+        TextInput,
+        FormTag,
+    },
+    mounted () {
+        (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+ 
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+            form.classList.add('was-validated')
+            }, false)
+        })
+        })()
+    }
+}
+</script>
